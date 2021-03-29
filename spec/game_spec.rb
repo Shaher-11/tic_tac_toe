5e06677f848f76195366efbae8ff9cb1a56bb160
Shaher-11 <shahershamroukh@gmail.com>
2021-03-29 19:14:13 +0200

    Adds tests covering nefative scenarios and use require_relative instaed of require

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,13 @@
-require './bin/main'
+require_relative('../bin/main')
 # rubocop: disable Style/MixinUsage
 
 describe Board do
   describe '.validate_name' do
     it 'validates the players names' do
       expect(Board.validate_name('al')).to eq(false)
+    end
+    it 'validates the players names' do
+      expect(Board.validate_name('shaher')).to eq(true)
     end
   end
 end
@@ -18,14 +21,23 @@ describe '#calc_tak_turns' do
 end
 
 describe '#test_move' do
-  it 'tests returns flase if the given number betwen 1 and 9' do
+  it 'tests returns true if the given number betwen 1 and 9' do
     @tak_turns = [1, 2, 3]
     expect(test_move(4)).to eq(true)
+  end
+  it 'tests returns flase if the given number is not betwen 1 and 9' do
+    @tak_turns = [11, 12]
+    expect(test_move(12)).to eq(false)
   end
 
   it 'tests returns flase if the given number betwen 1 and 9 and included in the give arr' do
     @tak_turns = [1, 2, 3]
     expect(test_move(3)).to eq(false)
+  end
+
+  it 'tests returns flase if the given number betwen 1 and 9 and not included in the give arr' do
+    @tak_turns = [1, 2, 3]
+    expect(test_move(5)).to eq(true)
   end
 
   it 'return false if a number is not an integer' do
@@ -47,7 +59,14 @@ describe '#wining' do
     expect(@winning_options[0] - @player1_trn).to eq([])
   end
 
-  it 'returns false for wining method f the players turn array matches one of the winning options' do
+  it 'returns true if the players turn array does not match one of the winning options' do
+    @winning_options = [[1, 2, 3], [4, 5, 6]]
+    @player1_trn = [1, 2, 3, 4]
+    @player2_trn = [4, 5]
+    expect(@winning_options[0] - @player1_trn).to eq([])
+  end
+
+  it 'returns false for wining method of the players turn array matches one of the winning options' do
     @winning_options = [[1, 2, 3], [4, 5, 6]]
     @player1_trn = [1, 2]
     @player2_trn = [4, 5]
@@ -55,11 +74,26 @@ describe '#wining' do
     expect(wining).to be false
   end
 
+  it 'returns true for wining method of the players turn array matches one of the winning options' do
+    @winning_options = [[1, 2, 3], [4, 5, 6]]
+    @player1_trn = [1, 2]
+    @player2_trn = [4, 5]
+    @win = true
+    expect(wining).to be true
+  end
+
   it 'returns true for wining method if the players turn array matches one of the winning options' do
     @winning_options = [[1, 2, 3], [4, 5, 6]]
     @player1_trn = [1, 2, 3]
     @player2_trn = [4, 5]
     expect(wining).to be true if @winning_options - @player1_trn == []
+  end
+
+  it 'returns true for wining method if the players turn array matches one of the winning options' do
+    @winning_options = [[1, 2, 3], [4, 5, 6]]
+    @player1_trn = [1, 2]
+    @player2_trn = [4, 5]
+    expect(wining).to be false if @winning_options - @player1_trn == []
   end
 
   it 'returns true for wining method if the players turn array matches one of the winning options' do
