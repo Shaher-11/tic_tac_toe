e41fde33984a2224dc80096ae601c4d9dfdb2e8c
felix_vondee <felixvondee8@gmail.com>
2021-03-29 15:42:59 +0200

    Creates test for test_move method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,6 @@
 require'./bin/main'
 
+
 describe Board do
 describe ".validate_name" do
   it "validates the players names" do
@@ -8,9 +9,19 @@ describe ".validate_name" do
 end 
 end 
 
+describe Logic do
+  include Logic
   describe "#calc_tak_turns" do
     it "calculates the plyer turns" do
       expect(calc_tak_turns = Array.new()).to eq([])
     end
   end
+
+  describe "#test_move" do 
+    it "tests each players move" do
+      @tak_turns = [1, 2, 3]
+      expect(test_move(4)).to eq(true)
+    end
+  end
+end
  
